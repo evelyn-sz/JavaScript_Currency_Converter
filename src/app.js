a426e09777ac3dcc6785b3c83475b326d2dd7f24
evelyn-sz <evelynszarek@gmail.com>
2021-01-12 22:24:15 +0000

    minor tweaks

--- src/app.js
+++ src/app.js
@@ -4,16 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: "#app",
         data: {
-            currencies: []
+            currencies: [],
+            selectedCurrency: null
         },
         mounted() {
-
+            this.fetchRates()
         },
         computed: {
 
         },
         methods: {
-            
+            fetchRates: function() {
+                fetch("https://api.exchangeratesapi.io/latest")
+                .then(res => res.json())
+                .then(currencies => this.currencies = currencies)
+            },
+            selectCurrency: function(currencyIndex) {
+                this.selectedCurrency = this.currencies[currencyIndex]
+            }
         }
     })
 })
