1a1ad4b8146d8baf8a67de994b6f53399595170f
evelyn-sz <evelynszarek@gmail.com>
2021-01-12 20:39:10 +0000

    Installed all packages

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+    new Vue({
+        el: "#app",
+        data: {
+            currencies: []
+        },
+        mounted() {
+
+        },
+        computed: {
+
+        },
+        methods: {
+            
+        }
+    })
+})
